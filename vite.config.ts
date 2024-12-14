05934b7590e5ed39b8f234b157d94e46d23a0975
seville <amirrezasalimi0@gmail.com>
2024-12-14 17:24:53 +0330

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   preview: {
+    host: "0.0.0.0",
     port: 3000,
   },
 });
